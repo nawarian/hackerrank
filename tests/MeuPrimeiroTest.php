bc75cf1df6b6053e8cc77da8cab9d451304366e8
Nawarian <nickolas@phpsp.org.br>
2023-04-02 18:17:48 +0200

    Primeiro commit

---
+++ tests/MeuPrimeiroTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nawarian\Hackerrank\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MeuPrimeiroTest extends TestCase
+{
+    public function testMeuPrimeiroTest(): void
+    {
+        self::assertFalse(false);
+    }
+}
